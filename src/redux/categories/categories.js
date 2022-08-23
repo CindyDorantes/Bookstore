aeeecc324c1c967d20578f07c0916cfbae809a7d
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-23 13:30:18 -0500

    write and export categories Reducer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,1 +1,13 @@
 const CHECK_STATUS = 'check_status';
+const initialState = [];
+
+const statusReducer = (status = initialState, action) => {
+  switch (action.type) {
+    case CHECK_STATUS :
+      return "Under construction";
+    default :
+      return status;
+  }
+} 
+
+export default statusReducer;
