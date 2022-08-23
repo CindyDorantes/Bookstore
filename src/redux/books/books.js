61197818ddd05a0d462fd38b56a79713ce08d7dd
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-23 13:13:44 -0500

    write and export booksReducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,3 +1,15 @@
 const ADD_BOOK = 'add_book';
 const REMOVE_BOOK = 'remove_book';
 
+const bookReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_BOOK :
+      return ;
+    case REMOVE_BOOK:
+      return;
+    default :
+    return state;
+    }
+};
+
+export default bookReducer;
