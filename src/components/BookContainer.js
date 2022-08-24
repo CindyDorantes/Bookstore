5d7e8af212418afc4da65e8351e878689fd1e375
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-24 15:05:17 -0500

    change the default state in books reducer

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,53 +1,25 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-// import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import BookList from './BookList';
 import InputBook from './InputBook';
 
-class BookContainer extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      books: [
-        {
-          id: uuidv4(),
-          genre: 'Action',
-          title: 'The Hunger Games',
-          author: 'Suzanne Collins',
-          chapter: 'Chapter 17',
-          progress: '64%',
-        },
-        // {
-        //   id: uuidv4(),
-        //   genre: 'Science Fiction',
-        //   title: 'Dune',
-        //   author: 'Frank Herbert',
-        //   chapter: 'Chapter 3: "A Lesson Learned"',
-        //   progress: '8%',
-        // },
-        // {
-        //   id: uuidv4(),
-        //   genre: 'Economy',
-        //   title: 'Capital in the Twenty-First Century',
-        //   author: 'Thomas Piketty',
-        //   chapter: 'Introduction',
-        //   progress: '0%',
-        // },
-      ],
-    };
-  }
-
-  render() {
-    const { books } = this.state;
-    return (
-      <div>
-        <BookList books={books} />
-        <InputBook />
-      </div>
-    );
-  }
-}
+const BookContainer = () => {
+  const books = [{
+    id: uuidv4(),
+    genre: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    chapter: 'Chapter 17',
+    progress: '64%',
+  }];
+  return (
+    <div>
+      <BookList books={books} />
+      <InputBook />
+    </div>
+  );
+};
 
 // BookContainer.propTypes = { books: PropTypes.func.isRequired };
 
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,36 @@
+// import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'add_book';
 const REMOVE_BOOK = 'remove_book';
+const initialState = [
+  {
+    id: uuidv4(),
+    genre: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    chapter: 'Chapter 17',
+    progress: '64%',
+  },
+  {
+    id: uuidv4(),
+    genre: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+    chapter: 'Chapter 3: "A Lesson Learned"',
+    progress: '8%',
+  },
+  {
+    id: uuidv4(),
+    genre: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Thomas Piketty',
+    chapter: 'Introduction',
+    progress: '0%',
+  },
+];
 
-const bookReducer = (state = [], action) => {
+const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       const book = action.book;
